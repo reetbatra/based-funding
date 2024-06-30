b7b622a90f2e8f9356613a3fa3cb48d2ca040f41
Khawla <69622217+khawlahssn@users.noreply.github.com>
2024-07-01 00:15:15 +0400

    💄 update home page ui

--- web/app/home/HomePage.tsx
+++ web/app/home/HomePage.tsx
@@ -8,18 +8,19 @@ export default function HomePage() {
   return (
     <>
       <Header />
-      <main className="container mx-auto flex flex-col items-center space-y-16 px-4 py-24 md:px-8">
-        <h1 className="text-center text-3xl font-bold leading-relaxed text-mainGray md:text-4xl lg:text-5xl">
-          we believe investing in education is based. donate in crypto wherever you are.
+      <main className="container mx-auto flex flex-col items-center space-y-16 px-8 py-24">
+        <h1 className="pt-4 text-center text-5xl font-bold leading-relaxed text-black">
+          we believe investing in education is based. <br /> donate in crypto wherever you are.
         </h1>
-        <p className="text-center text-2xl md:text-3xl lg:text-4xl">
-          Join as a <span className="text-mainBlue">Student</span> or Investor
+        <p className="text-center text-4xl">
+          join as a <span className="italic text-mainBlue underline">student</span> or{' '}
+          <span className="italic text-mainBlue underline">donor</span>
         </p>
-        <div className="flex flex-col gap-4 md:flex-row md:gap-8 lg:gap-64">
-          <Button asChild variant="blue" className="h-full w-full p-4 text-lg md:text-xl">
+        <div className="flex gap-20">
+          <Button asChild variant="blue" className="h-full w-1/2 p-4 text-xl">
             <Link href="/create-profile">Apply for funding</Link>
           </Button>
-          <Button asChild variant="black" className="h-full w-full p-4 text-lg md:text-xl">
+          <Button asChild variant="black" className="h-full w-1/2 p-4 text-xl">
             <Link href="/projects">Fund a student</Link>
           </Button>
         </div>
